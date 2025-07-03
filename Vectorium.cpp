ca26b25b9298992ad9ce8851ab570deb37aa9162
Harry <harrycode@tome.mozmail.com>
2025-07-03 14:33:37 +0100

    Update gitignore, and added CMake files

---
+++ Vectorium.cpp
@@ -0,0 +1,12 @@
+﻿// Vectorium.cpp : Defines the entry point for the application.
+//
+
+#include "Vectorium.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
