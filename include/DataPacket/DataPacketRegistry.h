2449a418f51912449fe132763f8a05d6bed8c960
HR <harryread@outlook.com>
2025-07-13 14:52:58 +0100

    rewrite of datapacketregistry

--- include/DataPacket/DataPacketRegistry.h
+++ include/DataPacket/DataPacketRegistry.h
@@ -2,23 +2,33 @@
 #include <memory>
 #include <typeindex>
 #include <vector>
+#include <set>
+#include <unordered_map>
+
+class IDataPacketHandler;
 
 class ILogger;
+class DataPacket;
 
 /// <summary>
-/// DataPacketRegistry tracks all packet types, and the handlers defined for them
+/// DataPacketRegistry knows about all packet types, and the handlers subscribed to each
 /// </summary>
 class DataPacketRegistry
 {
 public:
 	DataPacketRegistry(std::shared_ptr<ILogger> log);
 
-	void                                registerDataPackets();
+	bool registerDataPacketHandler(std::type_index packetType, std::shared_ptr<IDataPacketHandler> handler);
+
+	bool registerWildcardHandler(std::shared_ptr<IDataPacketHandler> handler);
 	const std::vector<std::type_index>& getDataPacketTypes() const;
 
+	void dispatch(const DataPacket& packet);
 
 
 private:
-	std::vector<std::type_index> dataPacketTypes;
+	std::unordered_map<std::type_index, std::vector<std::shared_ptr<IDataPacketHandler>>> m_handlers;
+	std::vector<std::shared_ptr<IDataPacketHandler>> m_wildcardHandlers;
+	
 	std::shared_ptr<ILogger> logger;
 };
--- src/DataPacketRegistry.cpp
+++ src/DataPacketRegistry.cpp
@@ -1,17 +1,51 @@
 #include "DataPacket/DataPacketRegistry.h"
 #include <string>
 #include <utility>
+#include "DataPacket/DataPacket.h"
+#include "DataPacket/IDataPacketHandler.h"
+
 
 DataPacketRegistry::DataPacketRegistry(std::shared_ptr<ILogger> log) : logger(std::move(log))
 {
+
 }
 
-void DataPacketRegistry::registerDataPackets()
+bool DataPacketRegistry::registerDataPacketHandler(std::type_index packetType, std::shared_ptr<IDataPacketHandler> handler)
 {
-	dataPacketTypes = {typeid(int), typeid(float), typeid(std::string)};
+	
+	m_handlers[packetType].push_back(std::move(handler));
+	return true;
 }
 
-const std::vector<std::type_index>& DataPacketRegistry::getDataPacketTypes() const
+const std::vector<std::type_index> &DataPacketRegistry::getDataPacketTypes() const
 {
-	return dataPacketTypes;
+	return {};
+	//return dataPacketTypes;
+}
+
+bool DataPacketRegistry::registerWildcardHandler(std::shared_ptr<IDataPacketHandler> handler)
+{
+	m_wildcardHandlers.push_back(std::move(handler));
+    return true;
+}
+
+void DataPacketRegistry::dispatch(const DataPacket &packet)
+{
+	auto it = m_handlers.find(packet.payloadType);
+	if(it != m_handlers.end())
+	{
+		for(const auto& handler : it->second)
+		{
+			if(!handler) continue;
+			
+			handler->handle(packet);
+		}
+	}
+
+	for(const auto& handler : m_wildcardHandlers)
+	{
+		if(!handler) continue;
+		
+		handler->handle(packet);
+	}
 }
